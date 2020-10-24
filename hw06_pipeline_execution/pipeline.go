aa6763b708af91c2a3b645c485d1f4f72fb450b0
nimoism <dmitry.puhov@gmail.com>
2020-10-24 10:14:26 +0300

    HW06 implement ExecutePipeline, add tests

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = terminator(stage(out), done)
+	}
+	return out
+}
+
+func terminator(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case value, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- value:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
 }
