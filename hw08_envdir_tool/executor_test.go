372832a267615c379fb9c2ed280240314ff977ab
Dmitry Puhov <dmitry.puhov@gmail.com>
2020-11-17 10:09:53 +0300

    HW08 Envdir tool (#8)
    * HW08 Envdir tool completed
    
    * Fix go.mod module path
    
    * Use log.Fatal instead of panic in main.go
    
    * Use ioutil.ReadDir() instead of os.File.Readdirnames(),
    Use bufio.Reader.ReadLine() instead of bufio.Scanner
    
    * Make ReadDir errors more verbose

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,55 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env, err := ReadDir("testdata/env")
+	var exitCode int
+	for _, tst := range []struct {
+		cmd  []string
+		out  string
+		code int
+	}{
+		{
+			cmd:  []string{"sh", "-c", "echo $HELLO $BAR"},
+			out:  "\"hello\" bar\n",
+			code: 0,
+		},
+		{
+			cmd:  []string{"wrong-cmd"},
+			out:  "",
+			code: ExitCodeIOError,
+		},
+		{
+			cmd:  []string{},
+			out:  "",
+			code: ExitCodeCommandNotFound,
+		},
+	} {
+		tst := tst
+		var buf bytes.Buffer
+		var r *os.File
+		require.NoError(t, err)
+		func() {
+			var w *os.File
+			r, w, err = os.Pipe()
+			defer w.Close()
+			origStdout := os.Stdout
+			defer func() { os.Stdout = origStdout }()
+			os.Stdout = w
+			require.NoError(t, err)
+			exitCode = RunCmd(tst.cmd, env)
+		}()
+		_, err = buf.ReadFrom(r)
+		require.NoError(t, err)
+		require.Equal(t, tst.code, exitCode)
+		require.Equal(t, tst.out, buf.String())
+
+	}
 }
