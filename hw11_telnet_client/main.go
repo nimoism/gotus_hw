827dea42bddbb9b5940b06db7f2278c824747079
Dmitry Puhov <dmitry.puhov@gmail.com>
2021-01-19 15:35:37 +0300

    HW11 Telnet Client (#11)
    * HW11 Telnet Client
    
    * Fix handling top errors
    
    * Update top error handling,
    Make closing send/receive notification by channel instead of CancelContext
    
    * Change send/receive results handle to chan

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,46 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "")
+	flag.Parse()
+	if flag.NArg() < 2 {
+		log.Fatal("Params `host` and `port` are mandatory")
+	}
+	address := net.JoinHostPort(flag.Arg(0), flag.Arg(1))
+
+	tc := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+	if err := tc.Connect(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer tc.Close()
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT)
+	defer signal.Stop(sigCh)
+
+	resultCh := make(chan error)
+	run := func(f func() error) {
+		resultCh <- f()
+	}
+	go run(tc.Send)
+	go run(tc.Receive)
+
+	select {
+	case <-sigCh:
+	case err := <-resultCh:
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
